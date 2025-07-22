45d1e6cf0c6451e974f8e2595cf41ae1bf257a73
rizabolukbasi <78274664+rizabolukbasi@users.noreply.github.com>
2025-07-22 15:45:22 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 // firebase.js
 
-// Firebase config (seninkinden alındı)
+// Firebase yapılandırması (senin projen)
 const firebaseConfig = {
   apiKey: "AIzaSyDskVgpbV3nJvYW0V0xcxMV2DKWh5nvQCE",
   authDomain: "test-c4a3f.firebaseapp.com",
   projectId: "test-c4a3f",
-  storageBucket: "test-c4a3f.appspot.com",  // ❗ burayı düzelt!
+  storageBucket: "test-c4a3f.appspot.com", // DÜZELTİLDİ
   messagingSenderId: "785148464489",
   appId: "1:785148464489:web:1f57b3675ff5d443be81f8",
   measurementId: "G-NDZVHGY54F"
