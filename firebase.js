75f7e066db3dbff053a950d5ad27e5636008a0eb
rizabolukbasi <78274664+rizabolukbasi@users.noreply.github.com>
2025-07-22 15:42:52 +0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
+// firebase.js
 
-// Firebase konfigürasyonu
+// Firebase config (seninkinden alındı)
 const firebaseConfig = {
   apiKey: "AIzaSyDskVgpbV3nJvYW0V0xcxMV2DKWh5nvQCE",
   authDomain: "test-c4a3f.firebaseapp.com",
   projectId: "test-c4a3f",
-  storageBucket: "test-c4a3f.appspot.com",
+  storageBucket: "test-c4a3f.appspot.com",  // ❗ burayı düzelt!
   messagingSenderId: "785148464489",
   appId: "1:785148464489:web:1f57b3675ff5d443be81f8",
   measurementId: "G-NDZVHGY54F"
 };
-// Initialize Firebase
+
+// Firebase'i başlat
 firebase.initializeApp(firebaseConfig);
-const storage = firebase.storage();
