199085b6fb2683e920603189422d4ab0c3d6e238
rizabolukbasi <78274664+rizabolukbasi@users.noreply.github.com>
2025-08-25 18:08:54 +0300

    Update upload.js

--- upload.js
+++ upload.js
@@ -16,7 +16,7 @@ async function uploadImage() {
     formData.append("cloud_name", "dlgw466il"); // Buraya kendi Cloudinary hesabını yaz
 
     try {
-      const response = await fetch("https://api.cloudinary.com/v1_1/senin_cloud_name/image/upload", {
+      const response = await fetch("https://api.cloudinary.com/v1_1/dlgw466il/image/upload", {
         method: "POST",
         body: formData
       });
